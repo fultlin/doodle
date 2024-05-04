099abbb1dab15dbed861f2f36247bbff8d652ccf
fultlin <ugolok.kita@hmail.com>
2024-05-04 11:17:59 +0300

    finish about section

---
+++ src/components/assets/Arrow.jsx
@@ -0,0 +1,9 @@
+const Arrow = ({className}) => {
+    return (
+        <svg width="314" height="256" viewBox="0 0 314 256" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+            <path d="M308.072 5.80496C151.422 9.90314 -116.127 63.1893 66.826 243.558M61.2918 202.171L75.6215 248.609L35.9615 250.38" stroke="#72C007" stroke-width="10" stroke-linecap="round" />
+        </svg>
+    )
+}
+
+export default Arrow
