8dba6aa194ceb741dda41b5ad899d42c07e2ad6f
Denis Andronov <denisunderonov@gmail.com>
2024-05-05 13:21:51 +0300

    did news and require sections

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,8 @@
-import About from "./about/About"
-import FirstScreen from "./firstScreen/FirstScreen"
-import Projects from "./Projects/Prokects"
+import About from "./about/About";
+import FirstScreen from "./firstScreen/FirstScreen";
+import Projects from "./Projects/Prokects";
+import Require from "./require/require";
+import News from "./news/news";
 
 const Main = () => {
     return (
@@ -16,9 +18,22 @@ const Main = () => {
                     <About />
                 </div>
             </section>
+
             <section className="section-projects">
                 <div className="wrapper main">
                     <Projects />
+                </div>
+            </section>
+
+            <section className="news">
+                <div className="wrapper main news__container">
+                    <News />
+                </div>
+            </section>
+
+            <section className="require">
+                <div className="wrapper main require__contanier">
+                    <Require />
                 </div>
             </section>
         </>
